cc4195d53b973853a0dc5fa4f62cc654fdb6bf8b
Ervan Dharmawan <ervandharmawanhalim@gmail.com>
2022-03-24 12:59:31 +0700

    fix CORS

--- go.mod
+++ go.mod
@@ -3,7 +3,7 @@ module github.com/ervanhohoho/lsip-mock-be
 go 1.15
 
 require (
-	github.com/gin-contrib/cors v1.3.1 // indirect
+	github.com/gin-contrib/cors v1.3.1
 	github.com/gin-gonic/gin v1.7.7
 	github.com/go-openapi/swag v0.21.1 // indirect
 	github.com/go-playground/validator/v10 v10.10.0 // indirect
@@ -13,9 +13,9 @@ require (
 	github.com/mailru/easyjson v0.7.7 // indirect
 	github.com/mattn/go-isatty v0.0.14 // indirect
 	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
-	github.com/swaggo/files v0.0.0-20210815190702-a29dd2bc99b2 // indirect
-	github.com/swaggo/gin-swagger v1.4.1 // indirect
-	github.com/swaggo/swag v1.7.9 // indirect
+	github.com/swaggo/files v0.0.0-20210815190702-a29dd2bc99b2
+	github.com/swaggo/gin-swagger v1.4.1
+	github.com/swaggo/swag v1.7.9
 	github.com/ugorji/go v1.2.6 // indirect
 	golang.org/x/crypto v0.0.0-20220214200702-86341886e292 // indirect
 	golang.org/x/sys v0.0.0-20220209214540-3681064d5158 // indirect
@@ -23,6 +23,6 @@ require (
 	golang.org/x/tools v0.1.9 // indirect
 	google.golang.org/protobuf v1.27.1 // indirect
 	gopkg.in/yaml.v2 v2.4.0 // indirect
-	gorm.io/driver/mysql v1.2.3 // indirect
-	gorm.io/gorm v1.22.5 // indirect
+	gorm.io/driver/mysql v1.2.3
+	gorm.io/gorm v1.22.5
 )
--- main.go
+++ main.go
@@ -1,6 +1,9 @@
 package main
 
 import (
+	"os"
+	"time"
+
 	"github.com/ervanhohoho/lsip-mock-be/accessor"
 	"github.com/ervanhohoho/lsip-mock-be/controller"
 	docs "github.com/ervanhohoho/lsip-mock-be/docs"
@@ -8,14 +11,20 @@ import (
 	"github.com/gin-gonic/gin"
 	swaggerfiles "github.com/swaggo/files"
 	ginSwagger "github.com/swaggo/gin-swagger"
-	"os"
-	"time"
 )
 
 func main() {
 	port := os.Getenv("PORT")
 	accessor := accessor.Initialize("103.15.172.2", "primanet_lsip_mock", "primanet_lsip_user", "lsipmock2021")
 	r := gin.Default()
+	r.Use(cors.New(cors.Config{
+		AllowOrigins:     []string{"https://internship-kemenkes.vercel.app"},
+		AllowMethods:     []string{"POST", "GET", "PATCH", "DELETE", "OPTIONS"},
+		AllowHeaders:     []string{"Origin", "Content-Length", "Content-Type", "User-Agent", "Referrer", "Host", "Token"},
+		ExposeHeaders:    []string{"Content-Length"},
+		AllowCredentials: true,
+		MaxAge:           12 * time.Hour,
+	}))
 	r.GET("/ping", func(c *gin.Context) {
 		c.JSON(200, gin.H{
 			"message": "pong",
@@ -25,17 +34,9 @@ func main() {
 	controller.InitUtilController(r, &accessor)
 	docs.SwaggerInfo_swagger.BasePath = "/"
 	r.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerfiles.Handler))
-	r.Use(cors.New(cors.Config{
-		AllowOrigins:     []string{"https://internship-kemenkes.vercel.app"},
-		AllowMethods:     []string{"POST", "GET", "PATCH", "DELETE", "OPTIONS"},
-		AllowHeaders:     []string{"Origin"},
-		ExposeHeaders:    []string{"Content-Length"},
-		AllowCredentials: true,
-		MaxAge:           12 * time.Hour,
-	}))
 	if port != "" {
 		r.Run(":" + port)
 	} else {
-		r.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
+		r.Run(":3000") // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
 	}
 }
